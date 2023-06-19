b7667034af4011167218ff8c04bb731caa00ae29
muthugit <contact@muthupandian.in>
2023-06-19 12:17:56 +0530

    v0.0.7

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='authit',
-    version='0.0.6',
+    version='0.0.7',
     description='An identity platform',
     author='Muthu',
     author_email='contact@muthupandian.in',
     packages=['authit'],
+    url='https://github.com/muthugit/authit',
     install_requires=[
         "bcrypt"
     ],
