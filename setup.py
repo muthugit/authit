4673bbb1d47b5dc8b06dcce13a9d0c026b0e47af
muthugit <contact@muthupandian.in>
2023-06-19 12:14:28 +0530

    v0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='authit',
-    version='0.0.5',
+    version='0.0.6',
     description='An identity platform',
     author='Muthu',
     author_email='contact@muthupandian.in',
