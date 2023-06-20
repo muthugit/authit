1c730adfb69a498280ea197d916256bc1f1466b2
muthugit <contact@muthupandian.in>
2023-06-20 10:31:23 +0530

    added

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+# html_theme = 'classic'
 # html_theme = 'jupyter-sphinx-theme'
 # html_theme = 'sphinx_materialdesign_theme'
 # html_theme = 'traditional'
